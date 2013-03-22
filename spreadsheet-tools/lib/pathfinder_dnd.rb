107e5ddc16f52a2f3668b19ef6a96b2a4e0187e7
Jake Teton-Landis <just.1.jake@gmail.com>
2013-03-22 16:48:58 -0700

    version string bump

--- spreadsheet-tools/lib/pathfinder_dnd.rb
+++ spreadsheet-tools/lib/pathfinder_dnd.rb
@@ -1,3 +1,8 @@
+# A simple D&D runtime that connects to Google Docs for your character data
+# Also loads personal functions from ~/.config/pathfinder/rc.rb
+module Pathfinder
+    VERSION = '0.1.3 - "User Functions"'
+end
 require 'pathfinder_dnd/character_sheet'
 require 'pathfinder_dnd/state_manager'
 
